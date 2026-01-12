705887a88ec86b8abae9a64e59e70e334a480488
malanak <malanakgames@seznam.cz>
2026-01-12 10:36:49 +0100

    Goose migrations

---
+++ migrations/20260112091702_create_message.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+CREATE TABLE "Message" (
+  "ID" SERIAL PRIMARY KEY,
+  "content" VARCHAR(1000) NOT NULL
+);
+-- +goose StatementEnd
+--
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+
+DROP TABLE "Message";
+-- +goose StatementEnd
